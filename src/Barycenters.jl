0946dbc2a46abe35a1509125558c85e7cc2e4df1
David Gentile <dc.gentile18@gmail.com>
2025-11-18 18:48:07 -0500

    changed KProj scheme to use autodiff

--- src/Barycenters.jl
+++ src/Barycenters.jl
@@ -17,7 +17,7 @@ function step_direction(ν, M, weights, Q; sstate=nothing, tol=1e-10, n_steps=10
     p = size(M, 2)
     for i=1:p
         gamma, _ = isnothing(sstate) ? BBD(Q, ν, M[:, i], N = n_steps, tol=tol) : BBD(Q, sstate, ν, M[:, i], N = n_steps, tol=tol)
-        tangent_vector = tangent_vector + weights[i] * (gamma.vector.m[0,:,:])
+        tangent_vector = tangent_vector + weights[i] * (gamma.vector.m[1,:,:])
     end
     return tangent_vector
 end
@@ -46,12 +46,16 @@ function barycenter(M, weights, Q;
                     h=0.1, maxiters=100, tol=1e-8, geodesic_tol=1e-10, geodesic_steps=100)
     ν_old = ones(size(Q,1)) # inital condition of flow
     ν_new = ones(size(Q,1)) # inital condition of flow
-    for _=1:maxiters
+    for k =1:maxiters
         δJ = step_direction(ν_old, M, weights, Q, sstate=sstate, tol=geodesic_tol, n_steps=geodesic_steps)
-        ν_new .= ν_old .+ h * graph_divergence(Q, metric_tensor(ν_old) .* δJ)
-        if sqrt(sum((ν_new - ν_old).^2)) < tol
+        ν_new = ν_old .- h * graph_divergence(Q, δJ)
+        norm_diff = sqrt(sum((ν_new - ν_old).^2))
+        if norm_diff < tol
+            println("finished in $(k) iterations")
             break
         else 
+            println("iteration $(k): normdiff=$(norm_diff)")
+            println("measure: $(ν_new)")
             ν_old = ν_new
         end
     end
@@ -60,12 +64,35 @@ end
 
 
 """
-    coordinates(ν, M, Q, maxiters=100, tol=1e-8, h=0.01)
+    coordinates(ν, M, Q)
 
 Description of the function.
 
 #TODO
 """
-function coordinates(ν, M, Q, maxiters=100, tol=1e-8, h=0.01)
-    error("Not Implemented")
+function analysis(ν, M, Q; N=32, tol=1e-10)
+    p = size(M, 2)
+
+    # compute the tangent vectors of the geodesics from the target measure to each reference
+    tangent_vectors = [BBD(Q, ν, M[:,i], N=N, tol=tol)[1].vector.m[1,:,:] for i=1:p]
+    g = metric_tensor(ν)
+
+    # form the matrix A for the QP
+    A = zeros(p,p)
+    for i=1:p, j=i:p
+        A[i,j] = A[j,i] = sum(tangent_vectors[i] .* tangent_vectors[j] .* g)
+    end
+
+    println(A)
+
+    # solve the QP
+    n = size(A, 1)
+    x = Variable(n)
+    problem = minimize(quadform(x, A))
+    # Simplex constraints
+    problem.constraints = vcat(problem.constraints, [x >= 0])
+    problem.constraints = vcat(problem.constraints, [sum(x) == 1])
+    
+    solve!(problem, SCS.Optimizer)
+    x.value  # optimal solution
 end
--- src/Experiments.jl
+++ src/Experiments.jl
@@ -1,6 +1,9 @@
 using SparseArrays
-include("utils.jl")
+using LinearAlgebra
+using JLD2
+using Convex, SCS
 include("tests/Inclusion.jl")
+include("CommonGraphs.jl")
 
 """
     gromov_convergence(N, n)
@@ -49,31 +52,22 @@ Description of the function.
 
 #TODO
 """
-function diracs_on_triangle(;N = 128, ε = 0., tol=1e-10, σ=0.5, τ=0.5, verbose=false)
-    edge_list = [(1,2), (2, 3), (3, 1)]
-
-    Q, π = markov_chain_from_edge_list(edge_list)
+function diracs_on_triangle(;N = 128, tol=1e-10, σ=0.5, τ=0.5, verbose=false)
+    Q, sstate = triangle_markov_chain()
     V = size(Q,1)
-    
-    π = steady_state_from_adjacency(Q)
     μ = zeros(V)
     ν = zeros(V)
-    μ[1] = 1/π[1]
-    ν[3] = 1/π[3]
+    μ[1] = 1/sstate[1]
+    ν[3] = 1/sstate[3]
 
-    γ, d = BBD(Q, μ + ε * a, ν + ε * b, N=N, tol=tol, verbose=verbose, σ=σ, τ=τ)
+    γ, d = BBD(Q, μ, ν, N=N, tol=tol, verbose=verbose, σ=σ, τ=τ)
     return (γ, d)
 end
 
 
 function triangle_with_tail(; N=128, tol=1e-10, verbose=false, σ=0.5, τ=0.5)
-
-    edge_list = [(1,2), (2, 3), (3, 1), (3, 4)]
-
-    Q, π = markov_chain_from_edge_list(edge_list)
+    Q, sstate = triangle_with_tail_markov_chain()
     V = size(Q,1)
-    
-    π = steady_state_from_adjacency(Q)
     μ = zeros(V)
     ν = zeros(V)
     μ[1] = 1/π[1]
@@ -86,20 +80,13 @@ end
 
 
 function diracs_on_prism(;N=100, tol=1e-10, σ=0.5, τ=0.5, verbose=false)
-
-    edge_list = [
-        (1,2), (2, 3), (3, 1),
-        (1,4), (2, 5), (3, 6),
-        (4,5), (5, 6), (6, 4)
-    ]
-
-    Q, π = markov_chain_from_edge_list(edge_list)
+    Q, sstate = triangular_prism_markov_chain()
     V = size(Q,1)
 
     μ = zeros(V)
     ν = zeros(V)
-    μ[1] = 1/π[1]
-    ν[2] = 1/π[2]
+    μ[1] = 1/sstate[1]
+    ν[2] = 1/sstate[2]
 
     γ, d = BBD(Q, μ, ν, N=N, verbose=verbose, tol=tol, σ=σ, τ=τ)
     return (γ, d)
@@ -115,15 +102,13 @@ Description of the function.
 #TODO
 """
 function diracs_on_square(;N=128, ε=0., verbose=false, tol=1e-6, σ=0.5, τ=0.5)
-    edge_list = [(1,2), (2, 3), (3, 4), (4, 1)]
-
-    Q, π = markov_chain_from_edge_list(edge_list)
+    Q, sstate = square_markov_chain()
     V = size(Q,1)
 
     μ = zeros(V)
     ν = zeros(V)
-    μ[1] = 1/π[1]
-    ν[2] = 1/π[2]
+    μ[1] = 1/sstate[1]
+    ν[2] = 1/sstate[2]
 
     a = [-1; 1/3; 1/3; 1/3;]
     b = [1/3; -1; 1/3; 1/3;]
@@ -141,9 +126,7 @@ Description of the function.
 #TODO
 """
 function diracs_on_T(;N=128, ε=0., verbose=false, tol=1e-6, σ=0.5, τ=0.5)
-    edge_list = [(1,2), (2, 3), (2, 4)]
-
-    Q, π = markov_chain_from_edge_list(edge_list)
+    Q, sstate = T_markov_chain()
     V = size(Q,1)
 
     μ = zeros(V)
@@ -157,19 +140,13 @@ function diracs_on_T(;N=128, ε=0., verbose=false, tol=1e-6, σ=0.5, τ=0.5)
 end
  
 function diracs_on_double_T(;N=128, ε=0., verbose=false, tol=1e-6, σ=0.5, τ=0.5)
-    edge_list = [
-        (1,2), (2, 3), (2, 4),
-        (1,5), (2, 6), (3, 7), (4, 8),
-        (5,6), (6, 7), (6, 8),
-    ]
-
-    Q, π = markov_chain_from_edge_list(edge_list)
+    Q, sstate = double_T_markov_chain()
     V = size(Q,1)
 
     μ = zeros(V)
     ν = zeros(V)
-    μ[1] = 1/π[1]
-    ν[3] = 1/π[3]
+    μ[1] = 1/sstate[1]
+    ν[3] = 1/sstate[3]
     γ, d = BBD(Q, μ, ν, N=N, verbose=verbose, tol=tol, σ=σ, τ=τ)
     return (γ, d)
 end
@@ -183,74 +160,46 @@ Description of the function.
 #TODO
 """
 function diracs_on_grid(; N=128, ε=0., tol=1e-6, σ=0.5, τ=0.5, verbose=false)
-
-    edge_list = [
-        (1, 2), (2, 3), (3, 4),
-        (3, 4), (4, 5), (5, 6),
-        (6, 7), (7, 8), (8, 1),
-        (9, 2), (9, 4), (9, 6), (9, 8)
-    ]
-
-    Q, π = markov_chain_from_edge_list(edge_list)
+    Q, sstate = grid_markov_chain()
     V = size(Q,1)
 
     μ = zeros(V)
     ν = zeros(V)
 
-    μ[2] = 1/π[2]
-    ν[6] = 1/π[6]
+    μ[2] = 1/sstate[2]
+    ν[6] = 1/sstate[6]
 
     γ, d = BBD(Q, μ, ν, N=N, verbose=verbose, tol=tol, σ=σ, τ=τ)
     return (γ, d)
 
 end
 
-function diracs_on_cube(; N=128, ε=0., tol=1e-6, σ=0.5, τ=0.5, verbose=false)
 
-    edge_list = [
-        (1, 2), (2, 3), (3, 4), (4, 1),
-        (1, 5), (2, 6), (3, 7), (4, 8),
-        (5, 6), (6, 7), (7, 8), (8, 5)
-    ]
 
-    Q, π = markov_chain_from_edge_list(edge_list)
+function diracs_on_cube(; N=128, ε=0., tol=1e-10, σ=0.5, τ=0.5, verbose=false)
+    Q, sstate = cube_markov_chain()
     V = size(Q,1)
 
     μ = zeros(V)
     ν = zeros(V)
 
-    μ[1] = 1/π[1]
-    ν[2] = 1/π[2]
-    #ν[7] = 1/π[7]
+    μ[1] = 1/sstate[1]
+    #ν[2] = 1/sstate[2]
+    ν[7] = 1/sstate[7]
 
     γ, d = BBD(Q, μ, ν, N=N, verbose=verbose, tol=tol, σ=σ, τ=τ)
     return (γ, d)
 end
 
 function diracs_on_hypercube(; N=128, ε=0., tol=1e-6, σ=0.5, τ=0.5, verbose=false)
-
-    edge_list = [
-        (1, 2), (2, 3), (3, 4), (4, 1),
-        (1, 5), (2, 6), (3, 7), (4, 8),
-        (5, 6), (6, 7), (7, 8), (8, 5),
-
-        (1,9), (2, 10), (3, 11), (4, 12),
-        (5, 13), (6, 14), (7, 15), (8, 16),
-
-        (9, 10), (10, 11), (11, 12), (12, 9),
-        (9, 13), (10, 14), (11, 15), (12, 16),
-        (13, 14), (14, 15), (15, 16), (16, 13)
-    ]
-
-    Q, π = markov_chain_from_edge_list(edge_list)
+    Q, sstate = hypercube_markov_chain()
     V = size(Q,1)
 
     μ = zeros(V)
     ν = zeros(V)
 
-
-    μ[1] = 1/π[1]
-    ν[15] = 1/π[15]
+    μ[1] = 1/sstate[1]
+    ν[15] = 1/sstate[15]
 
     γ, d = BBD(Q, μ, ν, N=N, verbose=verbose, tol=tol, σ=σ, τ=τ)
     return (γ, d)
@@ -263,25 +212,16 @@ Description of the function.
 #TODO
 """
 function barycenter_on_grid_EGD(; N=128, ε=0., verbose=false)
-    Q = Array(Tridiagonal(ones(7), zeros(8), ones(7)))
-    Q[1, 8] = 1
-    Q[8, 1] = 1
-    Q = hcat(Q, zeros(8))
-    Q = vcat(Q, zeros(9)')
-    Q[9,2] = Q[2,9] = Q[9,4] = Q[4,9] = Q[9,6] = Q[6,9] = Q[9,8] = Q[8,9] = 1
+    Q, sstate = grid_markov_chain()
 
-    for (idx, row) in enumerate(eachrow(Q))
-        z = sum(row)
-        Q[idx, :] /= z
-    end
-
-    π = steady_state_from_adjacency(Q)
     μ = zeros(9)
     ν = zeros(9)
     ρ = zeros(9)
+
     μ[1] = 1/π[1]
     ν[3] = 1/π[3]
     ρ[5] = 1/π[5]
+
     M = hcat(μ, ν, ρ)'
     λ = [1/3; 1/3; 1/3;]
 
@@ -291,7 +231,132 @@ function barycenter_on_grid_EGD(; N=128, ε=0., verbose=false)
     return optimize(F, x0, LBFGS(), Optim.Options(x_reltol=1e-6, iterations=128))
 end
 
-function inclusion_test(N)
-    c, d = diracs_on_square(N)
-    Script_K_pre_indicator(c)
+function cube_analysis(n;N=32, verbose=false, tol=1e-9, σ=0.5, τ=0.5)
+    Q, sstate = cube_markov_chain()
+    V = size(Q,1)
+
+    μ = zeros(V)
+    ν = zeros(V)
+
+    μ[1] = 1/sstate[1]
+    ν[7] = 1/sstate[7]
+
+    γ, d = BBD(Q, μ, ν, N=N, verbose=verbose, tol=tol, σ=σ, τ=τ)
+    targ = γ.vector.ρ[end ÷ n + 1,:]
+     
+    #M = hcat(μ, ν)
+    M = Diagonal(sstate.^-1)
+
+    analysis(targ, M, Q)
+    
+end
+
+function cube_synthesis(;N=32, verbose=false, tol=1e-9, σ=0.5, τ=0.5, maxiters=100)
+    Q, sstate = cube_markov_chain()
+    V = size(Q,1)
+
+    μ = zeros(V)
+    ν = zeros(V)
+
+    μ[1] = 1/sstate[1]
+    ν[7] = 1/sstate[7]
+
+    M = hcat(μ, ν)
+    
+    bar = barycenter(M, [0.5; 0.5], Q, geodesic_steps=N, geodesic_tol=tol, maxiters=maxiters)
+    
+end
+
+
+function cube_trouble(;N=32, tol=1e-9, verbose=false)
+    Q, sstate = cube_markov_chain()
+    V = size(Q,1)
+
+    μ = zeros(V)
+    ν = zeros(V)
+
+    μ[1] = 1/sstate[1]
+    ν[7] = 1/sstate[7]
+
+    M = hcat(μ, ν)
+    γ, d = BBD(Q, μ, ν, N=N, verbose=verbose, tol=tol)
+
+    targ = γ.vector.ρ[end ÷ 2 + 1,:]
+
+    γ1, _ = BBD(Q, targ, μ, N=N, tol=tol)
+    γ2, _ = BBD(Q, targ, ν, N=N, tol=tol)
+
+    return γ, γ1, γ2
+
+    
+end
+
+
+function objective_scaling(Q, μ, ν, l, u, filename)
+    M = hcat(μ, ν)
+    p = size(M, 2)
+
+    true_coords = [0.5; 0.5]
+
+    coord_errs = []
+    objs = []
+    normalized_objs = []
+    gradient_norms = []
+    gram_mat_norms = []
+
+    for k in l:u
+        N = 2^k
+        target = BBD(Q, μ, ν, N=N)[1].vector.ρ[end ÷ 2 + 1,:]
+
+        tangent_vectors = [BBD(Q, target, M[:,i], N=N, tol=1e-10)[1].vector.m[1,:,:] for i=1:p]
+        append!(gradient_norms, norm(0.5*(tangent_vectors[1] + 0.5*tangent_vectors[2])))
+        g = metric_tensor(target)
+        
+        # form the matrix A for the QP
+        A = zeros(p,p)
+        for i=1:p, j=i:p
+            A[i,j] = A[j,i] = sum(tangent_vectors[i] .* tangent_vectors[j] .* g)
+        end
+        
+        
+        # solve the QP
+        n = size(A, 1)
+        x = Variable(n)
+        problem = minimize(quadform(x, A))
+        # Simplex constraints
+        problem.constraints = vcat(problem.constraints, [x >= 0])
+        problem.constraints = vcat(problem.constraints, [sum(x) == 1])
+        
+        solve!(problem, SCS.Optimizer)
+        
+        append!(coord_errs, norm(x.value - true_coords) / norm(true_coords))  # optimal solution
+        append!(objs, x.value' * A * x.value)
+        append!(normalized_objs, (x.value' * A * x.value) / norm(A) )
+        append!(gram_mat_norms, norm(A))
+        
+    end
+
+    results = (coord_errs, objs, normalized_objs, gradient_norms, gram_mat_norms)
+    @save filename results
+
+end
+
+function cube_objective_scaling()
+    Q, sstate = cube_markov_chain()
+    V = size(Q,1)
+    μ = zeros(V)
+    ν = zeros(V)
+    μ[1] = 1/sstate[1]
+    ν[7] = 1/sstate[7]
+    objective_scaling(Q, μ, ν, 3, 8, "cube_scaling_results.jld2")
+end
+
+function triangle_objective_scaling()
+    Q, sstate = triangle_markov_chain()
+    V = size(Q,1)
+    μ = zeros(V)
+    ν = zeros(V)
+    μ[1] = 1/sstate[1]
+    ν[3] = 1/sstate[3]
+    objective_scaling(Q, μ, ν, 3, 8, "triangle_scaling_results.jld2")
 end
